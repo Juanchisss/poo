5c88dea80e9b6f76585834c45aa2a51ba468771b
Juanchisss <juan1019andres@gmail.com>
2024-03-29 09:15:24 -0500

    Actualizacion de codigo. Pt1 taller

---
+++ src/com/juanchis/taller/punto2/app/AppMaquinaDispensadora.java
@@ -0,0 +1,4 @@
+package com.juanchis.taller.punto2.app;
+
+public class AppMaquinaDispensadora {
+}
---
+++ src/com/juanchis/taller/punto2/dominio/MaquinaDispensadora.java
@@ -0,0 +1,4 @@
+package com.juanchis.taller.punto2.dominio;
+
+public class MaquinaDispensadora {
+}
