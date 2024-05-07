1d118f2838f26453e2b269609d08bed6c13b2e90
Juanchisss <juan1019andres@gmail.com>
2024-05-07 14:30:28 -0500

    correccion facturacion

--- src/com/juanchis/facturacion/app/AppFactu.java
+++ src/com/juanchis/facturacion/app/AppFactu.java
@@ -12,10 +12,11 @@ public class AppFactu {
     public static void main(String[] args) {
         Facturacion facturacion = new Facturacion();
 
-        facturacion.generarFactura("Alfredo", 950000, LocalDate.of(2024, 5, 1));
+        facturacion.generarFactura("Alfredo", 950_000, LocalDate.of(2024, 5, 1));
         facturacion.generarFactura("Alejandro", 3_160_000, LocalDate.of(2024, 5, 1));
         facturacion.generarFactura("Juanchisss", 5_000_000, LocalDate.of(2024, 4, 20));
         facturacion.generarFactura("Pedro", 10_000_000, LocalDate.now());
+        facturacion.generarFactura("Alejo", 500_000, LocalDate.of(2024, 10, 31));
 
         long total = facturacion.getTotalFacturacion();
         System.out.println("El total de la facturacion fue: " + total);
@@ -24,6 +25,6 @@ public class AppFactu {
         System.out.println("La factura mas cara es: " + masCara);
 
         List<Factura> sinIva = facturacion.getFacturasSinIva();
-        System.out.println(sinIva);
+        System.out.println("Las facturas sin iva son las siguientes: " + sinIva);
     }
 }
